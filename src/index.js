3cad9ea0e018de2d05884de0c504fbb319d248d8
NeuronPax <vetal3086@ukr.net>
2024-04-09 00:29:07 +0300

    create project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Root from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
