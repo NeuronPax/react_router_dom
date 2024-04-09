0fe3de2b3b96710a6410222664aff453c661df4a
NeuronPax <vetal3086@ukr.net>
2024-04-10 02:16:35 +0300

    refactoring...

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -2,63 +2,72 @@ import {Form} from 'react-router-dom'
 import avatar from '../assets/avatar.jpg'
 
 const Favorite = ({favorite}) => {
-  return (
-    <Form method='post'>
-      <button
-        className={`shadow-none hover:shadow-none ${favorite ? 'text-amber-500' : 'text-gray-400 hover:text-amber-500'}`}
-        name='favorite'
-        value={favorite ? 'false' : 'true'}
-        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}>
-          {favorite ? '★' : '☆'}
-      </button>
-    </Form>
-  )
+	return (
+		<Form method='post'>
+			<button
+				className={`shadow-none hover:shadow-none ${
+					favorite ? 'text-amber-500' : 'text-gray-400 hover:text-amber-500'
+				}`}
+				name='favorite'
+				value={favorite ? 'false' : 'true'}
+				aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}>
+				{favorite ? '★' : '☆'}
+			</button>
+		</Form>
+	)
 }
 
 const Contact = () => {
-  const contact = {
-    first: 'Your',
-    last: 'Name',
-    avatar,
-    favorite: true
-  }
-  return (
-    <div id='contact' className='flex'>
-      <div>
-        <img
-          className='rounded-3xl bg-gray-300 w-48 h-48 mr-8'
-          src={contact.avatar || null}
-          alt='' />
-      </div>
-      <div>
-        <h1 className='flex items-center gap-4 text-4xl font-bold'>
-          {contact.first || contact.last ? (
-            <>
-              {contact.first} {contact.last}
-            </>
-          ) : (
-            <i>No Name</i>
-          )}
-          <Favorite favorite={contact.favorite} />
-        </h1>
-        <div className='flex gap-2 my-4'>
-          <Form action='edit'>
-            <button type='submit'>Edit</button>
-          </Form>
-          <Form
-            method='post'
-            action='destroy'
-            onSubmit={e => {
-              if (!window.confirm('Please confirm you want to delete this record.')) {
-                e.preventDefault()
-              }
-            }}>
-              <button className='text-red-500' type='submit'>Delete</button>
-          </Form>
-        </div>
-      </div>
-    </div>
-  )
+	const contact = {
+		first: 'Your',
+		last: 'Name',
+		avatar,
+		favorite: true
+	}
+	return (
+		<div id='contact' className='flex'>
+			<div>
+				<img
+					className='rounded-3xl bg-gray-300 w-48 h-48 mr-8'
+					src={contact.avatar || null}
+					alt=''
+				/>
+			</div>
+			<div>
+				<h1 className='flex items-center gap-4 text-4xl font-bold'>
+					{contact.first || contact.last ? (
+						<>
+							{contact.first} {contact.last}
+						</>
+					) : (
+						<i>No Name</i>
+					)}
+					<Favorite favorite={contact.favorite} />
+				</h1>
+				<div className='flex gap-2 my-4'>
+					<Form action='edit'>
+						<button type='submit'>Edit</button>
+					</Form>
+					<Form
+						method='post'
+						action='destroy'
+						onSubmit={e => {
+							if (
+								!window.confirm(
+									'Please confirm you want to delete this record.'
+								)
+							) {
+								e.preventDefault()
+							}
+						}}>
+						<button className='text-red-500' type='submit'>
+							Delete
+						</button>
+					</Form>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Contact
--- src/routes/root.js
+++ src/routes/root.js
@@ -1,40 +1,51 @@
 import {Outlet, Link} from 'react-router-dom'
 
 const Root = () => {
-  return (
-    <div className='flex h-screen'>
-      <div id='sidebar' className='flex flex-col bg-gray-100 border-r-2 w-96'>
-        <div className='flex border-b-2 gap-2 py-4 px-8'>
-          <form id='search-form' className='flex-1' role='search'>
-            <input
-              id='q'
-              className='w-full'
-              aria-label='Search contacts'
-              placeholder='Search'
-              type='search'
-              name='q' />
-          </form>
-          <form method='post'>
-            <button type='submit'>New</button>
-          </form>
-        </div>
-        <nav className='pl-8'>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
-        </nav>
-        <h1 className='border-t-2 text-center font-medium py-4'>React Router Contacts</h1>
-      </div>
-      <div id='detail'>
-        <Outlet />
-      </div>
-    </div>
-  )
+	return (
+		<div className='flex h-screen'>
+			<div id='sidebar' className='flex flex-col bg-gray-100 border-r-2 w-96'>
+				<div className='flex border-b-2 gap-2 py-4 px-8'>
+					<form id='search-form' className='flex-1' role='search'>
+						<input
+							id='q'
+							className='w-full'
+							aria-label='Search contacts'
+							placeholder='Search'
+							type='search'
+							name='q'
+						/>
+					</form>
+					<form method='post'>
+						<button type='submit'>New</button>
+					</form>
+				</div>
+				<nav className='flex-1 py-4 px-8'>
+					<ul>
+						<li>
+							<Link
+								to={`/contacts/1`}
+								className='flex rounded-lg my-1 p-2 hover:bg-gray-200'>
+								Your Name
+							</Link>
+						</li>
+						<li>
+							<Link
+								to={`/contacts/2`}
+								className='flex rounded-lg p-2 hover:bg-gray-200'>
+								Your Friend
+							</Link>
+						</li>
+					</ul>
+				</nav>
+				<h1 className='border-t-2 text-center font-medium py-4'>
+					React Router Contacts
+				</h1>
+			</div>
+			<div id='detail' className='flex-1 py-8 px-16'>
+				<Outlet />
+			</div>
+		</div>
+	)
 }
 
 export default Root
